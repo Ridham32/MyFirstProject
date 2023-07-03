aabe601b54412ea9772894242dc9ebe2688192d1
Ridham32 <sharmaridham473@gmail.com>
2023-07-03 18:07:55 +0530

    printDataof1stScreen

--- app/src/main/java/com/ridhamSharma/myfirstproject/IntentscreenActivity.kt
+++ app/src/main/java/com/ridhamSharma/myfirstproject/IntentscreenActivity.kt
@@ -1,7 +1,12 @@
 package com.ridhamSharma.myfirstproject
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.TestLooperManager
+import android.service.autofill.TextValueSanitizer
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class IntentscreenActivity : AppCompatActivity() {
     var name =""
@@ -9,6 +14,12 @@ class IntentscreenActivity : AppCompatActivity() {
     var number = ""
      var marks=0f
     var percentage = 0.0
+    var tvName : TextView?= null
+    var tvCollegeinfo: TextView?= null
+    var tvPhonenumber: TextView?= null
+    var tvTotalmarks: TextView?= null
+    var tvPercentage: TextView?= null
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.intentscreen)
@@ -21,6 +32,18 @@ class IntentscreenActivity : AppCompatActivity() {
            // System.out.println("name $name college $college Phone number $number Total marks $marks percentage $percentage")
 
         }
+        tvName = findViewById(R.id.tvName)
+        tvName?.setText(name)
+        tvCollegeinfo = findViewById(R.id.tvCollegeinfo)
+        tvCollegeinfo?.setText(college)
+        tvPhonenumber = findViewById(R.id.tvPhonenumber)
+        tvPhonenumber?.setText(number)
+        tvTotalmarks= findViewById(R.id.tvTotalmarks)
+        tvTotalmarks?.setText(marks.toString())
+        tvPercentage = findViewById(R.id.tvPercentage)
+        tvPercentage?.setText(percentage.toString())
+
+
        System.out.println("name $name college $college Phone number $number Total marks $marks percentage $percentage")
 
 
