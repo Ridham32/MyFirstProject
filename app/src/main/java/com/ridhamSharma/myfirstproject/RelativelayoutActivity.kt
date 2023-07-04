a614990bc863ab76b103b19abec14b317e7de935
Ridham32 <sharmaridham473@gmail.com>
2023-07-04 17:36:31 +0530

    relativeLayout

---
+++ app/src/main/java/com/ridhamSharma/myfirstproject/RelativelayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.ridhamSharma.myfirstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativelayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.relativelayout)
+    }
+}
